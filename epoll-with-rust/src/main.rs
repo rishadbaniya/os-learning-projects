49dedea3d750ab8c49734c70eb551e7a27d5976a
rishadbaniya <rbdroid01@gmail.com>
2022-03-19 10:17:52 +0530

    update epoll

--- epoll-with-rust/src/main.rs
+++ epoll-with-rust/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use libc::open64;
+
+#[allow(unused_macros)]
+macro_rules! syscall {
+    ($fn: ident ( $($arg: expr),* $(,)* ) ) => {{
+        let res = unsafe { libc::$fn($($arg, )*) };
+        if res == -1 {
+            Err(std::io::Error::last_os_error())
+        } else {
+            Ok(res)
+        }
+    }};
 }
+
+fn main() {}
